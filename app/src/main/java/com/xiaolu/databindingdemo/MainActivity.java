7533ff15dffbb212cf5e384dfc9896b4e22dac48
rebornwhu <rebornwhu@gmail.com>
2015-10-04 15:18:21 -0700

    Get data binding works

--- app/src/main/java/com/xiaolu/databindingdemo/MainActivity.java
+++ app/src/main/java/com/xiaolu/databindingdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.xiaolu.databindingdemo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xiaolu.databindingdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
